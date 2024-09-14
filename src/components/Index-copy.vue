59ee8c35fd50589529d6cba0a5a295a7d7a5aabb
lixiaoshuai <xiaoshuai.li@istarshine.com>
2024-09-14 10:47:08 +0800

    首页

---
+++ src/components/Index-copy.vue
@@ -0,0 +1,26 @@
+<template>
+  <lay-space>
+    <lay-ripple>
+      <lay-button>click</lay-button>
+    </lay-ripple>
+    <lay-ripple type="out" borderRadius="2px">
+      <lay-button>click</lay-button>
+    </lay-ripple>
+  </lay-space>
+</template>
+
+<script>
+import { laySpace } from '@layui/layui-vue';
+
+export default {
+  name: 'SpaceExample',
+  components: {
+    laySpace
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router';
+import Layui from '@layui/layui-vue'
+import '@layui/layui-vue/lib/index.css';
+import 'animate.css';
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(Layui);
+app.mount('#app');
