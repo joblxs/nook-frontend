d41fe1bf58154a083bd0ffd50b928e3ab5526e92
lixiaoshuai <xiaoshuai.li@istarshine.com>
2024-09-14 14:33:22 +0800

    添加背景

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
+<!--  <GlobalBackground />-->
+  <iframe class="background-iframe" src="/GlobalBackground.html" frameborder="0"></iframe>
   <router-view />
 </template>
 
 <script>
-
+export default {
+  name: 'App'
+};
 </script>
 
 <style>
-
+body {color: #fff !important}
+.background-iframe {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+}
 </style>
