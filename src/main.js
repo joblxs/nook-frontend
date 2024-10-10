9bc5290ca22fab05783391acabd860de05bcc5ed
lxs <lxs@lxs>
2024-10-10 14:48:49 +0800

    去除element

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router';
 import Layui from '@layui/layui-vue'
-import ElementPlus from 'element-plus'
 import 'animate.css';
 import '@layui/layui-vue/lib/index.css';
 
 const app = createApp(App);
 app.use(router);
-app.use(ElementPlus);
 app.use(Layui);
 app.mount('#app');
